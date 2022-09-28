ae2bdd58f4a9b5a466fefa467fa8ce330032628a
LorenzoLotti <LorenzoLotti@users.noreply.github.com>
2022-09-28 16:00:03 +0200

    project setup

---
+++ server/src/main/java/it/fi/itismeucci/uppercaser/server/App.java
@@ -0,0 +1,13 @@
+package it.fi.itismeucci.uppercaser.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
